f3ed1a8f9741cc4737b7fde8319d7a53638892e1
javier <javiszumilo87@gmail.com>
2024-03-19 23:17:15 -0300

    login

---
+++ env.js
@@ -0,0 +1,8 @@
+export const uri = 'mongodb+srv://javiszumilo87:gcDE719B48EVkoJ9@cluster7.x4k0ov3.mongodb.net/'
+export const saltWord = 'secret'
+
+export const clientID = '63cdfbed3c2c73de9226'
+export const clientSecret = '1d58b4bac3f171e5265b6a89c09c63dc48805af2'
+export const callbackURL = 'http://localhost:8080/api/session/githubcallback'
+
+export const jwtKey = 'c0d3r'
